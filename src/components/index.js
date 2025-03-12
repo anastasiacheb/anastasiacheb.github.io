2778290d563b9f206314b2dda08eb9850ef30b56
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2025-03-12 03:48:42 +0300

    delete all

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Nav } from './Nav';
-export { default as Header } from './Header';
-export { default as Service } from './Service';
-export { default as Stack } from './Stack';
-export { default as Works } from './Works';
-export { default as Testi } from './Testi';
-export { default as Footer } from './Footer';
